5ec65afd9d880f99e6ba7ffe93ca3b19beff907a
todoi <1403724061@qq.com>
2017-07-27 01:09:24 +0800

    painter add pen eraser clear save textarea button

--- page-demos/painter/main.js
+++ page-demos/painter/main.js
@@ -1,12 +1,47 @@
-let board = document.querySelector('main.board');
-board.addEventListener('touchmove',function(e){
-    let clientX = e.touches[0].clientX;
-    let clientY = e.touches[0].clientY;
-    let div = document.createElement('div');
-    div.className += 'dot';
-    div.style.position = 'absolute';
-    div.style.top =  clientY-5+'px';
-    div.style.left = clientX-5+'px';
-    board.appendChild(div)
-    board.addEventListener('touchmove',function(e){
+let canvas = document.querySelector('#board');
+let html = document.documentElement;
+canvas.width = html.clientWidth;
+canvas.height = html.clientHeight;
+
+let action = 'pen'
+let pen = document.querySelector('#pen');
+let eraser = document.querySelector('#eraser')
+let clear = document.querySelector('#clear');
+
+let context = canvas.getContext('2d');
+let previousPoint
+
+pen.addEventListener('click',function(e){
+    action = 'pen';
+    e.currentTarget.classList.add('active')
 })
+eraser.addEventListener('click',function(e){
+    action = 'eraser';
+    e.currentTarget.classList.add('active')
+})
+
+canvas.addEventListener('touchstart',function(e){
+    previousPoint = e.touches[0];
+})
+
+canvas.addEventListener('touchmove',function(e){
+    let {clientX,clientY} = e.touches[0];
+    if (action === 'pen'){
+        if(previousPoint){
+            context.strokeStyle = 'red';
+            context.beginPath();
+            context.moveTo(previousPoint.clientX,previousPoint.clientY);
+            context.lineTo(clientX,clientY);
+            context.stroke();
+        }
+        previousPoint = e.touches[0]
+    }else if(action === 'eraser'){
+        context.clearRect(clientX-5,clientY-5,10,10);
+    }
+})
+
+clear.onclick = function(e){
+    e.currentTarget.classList.add('active')
+    context.clearRect(0,0,canvas.width,canvas.height);
+}
+
