f8a8a786cc577e4033f0e4f5076f7a707b03c3bb
todoi <1403724061@qq.com>
2017-08-10 16:40:27 +0800

    自动加载完成

--- page-demos/autoloading/node-server.js
+++ page-demos/autoloading/node-server.js
@@ -7,19 +7,31 @@ let fs = require('fs')
 let port = process.env.PORT || 8888
 
 let server = http.createServer(function(request,response){
-    console.log(request.method)
     let path = url.parse(request.url).pathname
     let query = querystring.parse(url.parse(request.url).query,'&','=')
     if (path === '/'){
-        let string = fs.readFileSync('./index.html')
-        response.statusCode = 200
-        response.setHeader('content-type','text/html;charset:utf-8')
-        response.end(string)
+        if (query['page'] === 'page1'){
+            let string = fs.readFileSync('./page1')
+            response.statusCode = 200
+            response.setHeader('content-type','text/html,charset:utf-8')
+            response.end(string)
+        }else if(query.page === 'page2'){
+            let string = fs.readFileSync('./page2')
+            response.statusCode = 200
+            response.setHeader('content-type','text/html,charset:utf-8')
+            response.end(string)
+        }else{
+            let string = fs.readFileSync('./index.html')
+            response.statusCode = 200
+            response.setHeader('content-type','text/html;charset:utf-8')
+            response.end(string)
+        }
     }else{
         response.statusCode = 404
         response.setHeader('content-type','text/plain')
         response.end("the page you request not exist")
     }
+    console.log(request.method + path)
 })
 console.log(`正在监听:${port}`)
 
