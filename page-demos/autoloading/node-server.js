26b254d84cc9142e88e5cd9d2df7ebc614a0ea27
todoi <1403724061@qq.com>
2017-08-15 14:11:21 +0800

    解决node-server 判断路径逻辑错误

--- page-demos/autoloading/node-server.js
+++ page-demos/autoloading/node-server.js
@@ -8,24 +8,21 @@ let port = process.env.PORT || 8888
 
 let server = http.createServer(function(request,response){
     let path = url.parse(request.url).pathname
-    let query = querystring.parse(url.parse(request.url).query,'&','=')
     if (path === '/'){
-        if (query['page'] === 'page1'){
-            let string = fs.readFileSync('./page1')
-            response.statusCode = 200
-            response.setHeader('content-type','text/html,charset:utf-8')
-            response.end(string)
-        }else if(query.page === 'page2'){
-            let string = fs.readFileSync('./page2')
-            response.statusCode = 200
-            response.setHeader('content-type','text/html,charset:utf-8')
-            response.end(string)
-        }else{
-            let string = fs.readFileSync('./index.html')
-            response.statusCode = 200
-            response.setHeader('content-type','text/html;charset:utf-8')
-            response.end(string)
-        }
+        let string = fs.readFileSync('./index.html')
+        response.statusCode = 200
+        response.setHeader('content-type','text/html;charset:utf-8')
+        response.end(string)
+    }else if (path === '/page1'){
+        let string = fs.readFileSync('./page1')
+        response.statusCode = 200
+        response.setHeader('content-type','text/html,charset:utf-8')
+        response.end(string)
+    }else if(path === '/page2'){
+        let string = fs.readFileSync('./page2')
+        response.statusCode = 200
+        response.setHeader('content-type','text/html,charset:utf-8')
+        response.end(string)
     }else{
         response.statusCode = 404
         response.setHeader('content-type','text/plain')
